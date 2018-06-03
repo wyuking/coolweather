83d673c305df05eae7b55f6794382eba684e362a
wangyu <1030106089@qq.com>
2018-06-03 19:50:16 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/choose_area.java
@@ -0,0 +1,13 @@
+package com.coolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class choose_area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_area);
+    }
+}
